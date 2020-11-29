a6119b149b95e70a2dba7e8f5d70110f7ff1dad6
kongames <kon_kostas@windowslive.com>
2020-11-29 03:11:38 +0200

    Created GetUserSelectedTweets.java for 2nd Requirement

---
+++ app/src/main/java/gr/uom/socialmediaaggregetor/AsyncTasks/GetUserSelectedTweets.java
@@ -0,0 +1,13 @@
+package gr.uom.socialmediaaggregetor.AsyncTasks;
+
+import android.os.AsyncTask;
+
+public class GetUserSelectedTweets extends AsyncTask<Void,Void,String> {
+
+
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        return null;
+    }
+}
